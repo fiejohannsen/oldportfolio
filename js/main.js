ea72789b454cdbe24e23fae3e582011affd4bd24
Fie Mølgaard  Johannsen <fiejohannsen@live.dk>
2019-10-08 21:32:55 +0200

    new styling

--- js/main.js
+++ js/main.js
@@ -87,3 +87,21 @@ function openDevice(evt, deviceName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+// Tabs
+function openDevice2(evt, deviceName) {
+  var i, tabcontent2, tablinks2;
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";
+  }
+  tablinks2 = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks2.length; i++) {
+    tablinks2[i].className = tablinks2[i].className.replace(" active", "");
+  }
+  document.getElementById(deviceName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen2").click();
